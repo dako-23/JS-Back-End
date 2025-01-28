59c419bb2e843f578eeebf7aa8bd2549cc606d18
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-01-28 13:52:31 +0200

    create movie model

--- Movie-Magic/src/index.js
+++ Movie-Magic/src/index.js
@@ -2,11 +2,23 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import routes from './routes.js';
 import showRating from './helpers/rating-helper.js';
+import mongoose from 'mongoose';
 
-
+//setup server
 const app = express();
 
+//setup DB
+try {
+    const uri = 'mongodb://localhost:27017/magic-movie-jan2025';
+    await mongoose.connect(uri)
 
+    console.log('connect to DB successfully')
+
+} catch (err) {
+    console.log(err.message);
+}
+
+//setup hbc
 app.engine('hbs', handlebars.engine(
     {
         extname: 'hbs',
@@ -19,9 +31,12 @@ app.engine('hbs', handlebars.engine(
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
 
+//setup express
 app.use(express.static('src/public'));
 app.use(express.urlencoded({ extended: false }));
 
+//setup roots
 app.use(routes)
 
+//start server
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
